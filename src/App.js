f31915ee30b9c6eaba718c188efeddc921cf555e
jMisch44 <jennimisch@gmail.com>
2021-08-30 15:51:04 -0400

    basic structures setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const todoArray = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false,
+  },
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +26,8 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>todo form</h1>
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
